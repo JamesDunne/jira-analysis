f02c913bdc0f398aaba709e7aca670131edbf187
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2018-11-08 12:27:04 -0500

    Split out Date type

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ type zonedTimestamp struct {
 }
 
 func (t *zonedTimestamp) UnmarshalJSON(buf []byte) error {
-	//                           "2017-12-15T11:02:01.443-0500"
 	tt, err := time.Parse("2006-01-02T15:04:05.999-0700", strings.Trim(string(buf), `"`))
 	if err != nil {
 		return err
@@ -92,52 +91,6 @@ type PagedIssues struct {
 	MaxResults int     `json:"maxResults"`
 	Total      int     `json:"total"`
 	Issues     []Issue `json:"issues"`
-}
-
-type Date struct {
-	time.Time
-}
-
-func DateOf(t time.Time) Date {
-	// Grab local date:
-	//_, zoneOffset := t.Zone()
-	l := t.Location()
-	y, m, d := t.Date()
-	// Build new date:
-	return Date{time.Date(y, m, d, 6, 0, 0, 0, l)}
-}
-
-func (date Date) NextDate() Date {
-	return DateOf(date.Time.Add(25 * time.Hour))
-}
-
-func (date Date) BusinessDaysUntil(until Date) int {
-	// Count weekdays, skipping weekends:
-	days := 0
-	d := date
-
-	_, startOffset := date.Zone()
-	_, untilOffset := until.Zone()
-	untilTime := until.In(date.Location()).Add(time.Duration(untilOffset-startOffset) * time.Second)
-	//fmt.Printf("from %s to %s\n", date.Time, untilTime)
-
-	for d.Time.Before(untilTime) {
-		//fmt.Printf("  %d %s\n", days, d)
-
-		days++
-		d = d.NextDate()
-
-		if d.Time.Weekday() == time.Saturday {
-			d = d.NextDate()
-		}
-		if d.Time.Weekday() == time.Sunday {
-			d = d.NextDate()
-		}
-	}
-
-	//fmt.Printf("  %d %s\n", days, d)
-
-	return days
 }
 
 type IssueList []*Issue
